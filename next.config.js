09ee07ff8c1c891bb9f83cf26c0f8cca596a817f
MEGAPASTELITO <man6anielloqq@gmail.com>
2023-10-01 20:59:41 -0400

    Este es el primer commit del proyecto.

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import ('next').NextConfig] */
+const nextConfig = {
+	images: {
+		domains: ["api-ninjas.com", "i.ibb.co"],
+	},
+
+	reactStrictMode: true,
+};
+module.exports = nextConfig;
